49730f802217e21310b2eb1ed70646c091bbf1f6
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-03 00:20:46 -0300

    add: config API

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-document.querySelector('.busca').addEventListener('submit', (event)=>{
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
         event.preventDefault();
 
         let input = document.querySelector('#searchInput').value
+        
+        if (input !== '') {
+            showWarning("Carregando...")
 
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=9c31ccf1b2f16a6e8baa8e773bcb5ae4&lang=pt-br`
+        
+        
+        }
 })
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg
+    
+}
