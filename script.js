bef5eafc54f2b467273bccea1bbb55179d0f40a6
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-03 00:29:10 -0300

    project finished

--- script.js
+++ script.js
@@ -42,8 +42,8 @@ function showInfo(json) {
     document.querySelector('.resultado').style.display = 'block';
 
     document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
-    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`; // Corrigido 'json.humidade' para 'json.temp'
-    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`; // Adicionado ponto e vírgula
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
 
     document.querySelector(".temp img").setAttribute("src", `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
 
