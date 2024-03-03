4d77426f3b52b568eac3d1bbe464b2a8af72d517
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-02 23:53:39 -0300

    add: added API weathermap

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ document.querySelector('.busca').addEventListener('submit', (event)=>{
         event.preventDefault();
 
         let input = document.querySelector('#searchInput').value
-        console.log(input)
 
 })
